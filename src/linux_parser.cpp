6168f73004f7ffb1ac40cd667689ff328955eb70
Nícholas André <nicholas.andre@10up.com>
2021-04-24 11:30:05 -0300

    Running and total processes

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -125,11 +125,43 @@ long LinuxParser::IdleJiffies() { return 0; }
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
 
-// TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses() { 
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  std::string line;
+  std::string key;
+  int value = 0;
 
-// TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+  if(stream.is_open()) {
+    while(std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      linestream >> key;
+      if(key == "processes") {
+        linestream >> value;
+      }
+    }
+  }
+
+  return value;
+}
+
+int LinuxParser::RunningProcesses() {
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  std::string line;
+  std::string key;
+  int value = 0;
+
+  if(stream.is_open()) {
+    while(std::getline(stream, line)) {
+      std::istringstream linestream(line);
+      linestream >> key;
+      if(key == "procs_running") {
+        linestream >> value;
+      }
+    }
+  }
+
+  return value;
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
